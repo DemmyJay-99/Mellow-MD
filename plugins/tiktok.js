16e49a0cb1adfec84c73d45b51cb4f2de33f0670
DemmyJay <demmyjyd@gmail.com>
2026-03-08 14:56:52 +0100

    Update tiktok

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -7,11 +7,19 @@ export default {
     execute: async (sock, msg, args) => {
         if(!args || args.length === 0) {
             await sock.sendMessage(msg.key.remoteJid, {
-                text: "invalid url"
+                text: "Usage: tiktok <TikTok URL> (or reply to a message with the URL)"
             })
             return
         }
-        let { video } = await ttdl(args[0]);
+        const url = args[0];
+        function isTikTokUrlRegex(url) {
+          const tiktokRegex = new RegExp(/^(https?:\/\/)?(www\.|m\.|vm\.)?tiktok\.com\b/);
+          return tiktokRegex.test(url);
+        }
+        if(!isTikTokUrlRegex(url)) {
+           await sock.sendMessage(msg.key.remoteJid, {text: "Invalid url"})
+        }
+        let { video } = await ttdl(url);
         await sock.sendMessage(msg.key.remoteJid,
             {video: {url: video}}
         )
