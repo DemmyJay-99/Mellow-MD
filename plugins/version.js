a615bf621627892beab313c81a26b26eff4f13ad
Mellow <demmyjyd@gmail.com>
2026-04-06 14:49:54 +0000

    Add version plugin

---
+++ plugins/version.js
@@ -0,0 +1,12 @@
+import p from '../package.json' with { type: 'json' };
+
+const  version = p.version;
+export default {
+    name: "version",
+    description: "Check the bot's current version",
+    isPublic: false,
+    category: "Utility",
+    execute: async (sock, msg, args) => {
+        await sock.sendMessage(msg.key.remoteJid, { text: `Mellow MD Version: ${version}` });
+    }
+}
