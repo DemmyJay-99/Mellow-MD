969f7cfebae6e3a35870a43474d55f5818b7b223
Mellow <demmyjyd@gmail.com>
2026-04-07 16:51:01 +0000

    Add gcdesc plugin

---
+++ plugins/gcdesc.js
@@ -0,0 +1,51 @@
+import normaliseLid from "../lib/normaliseLid.js";
+
+export default {
+  name: "gcdesc",
+  description: "Change the group description",
+  isPublic: false,
+  category: "Group",
+  execute: async (sock, msg, args) => {
+    const remoteJid = msg.key.remoteJid;
+    if (!remoteJid.endsWith("@g.us")) {
+      return sock.sendMessage(remoteJid, {
+        text: "This command only works in groups.",
+      });
+    }
+    const metadata = await sock.groupMetadata(remoteJid);
+    let senderJid = msg.key.participant || msg.key.remoteJid;
+    if (senderJid.endsWith("@lid")) {
+      let pn = await normaliseLid(sock, senderJid);
+      senderJid = pn + "@s.whatsapp.net";
+    }
+    const admins = metadata.participants
+      .filter((p) => p.admin)
+      .map((p) => p.id);
+    if (!admins.includes(senderJid) && !msg.key.fromMe) {
+      return sock.sendMessage(remoteJid, { text: "Admin only." });
+    }
+
+    const botJid = sock.user.id.split(":")[0] + "@s.whatsapp.net";
+
+    if (!admins.includes(botJid)) {
+      return sock.sendMessage(remoteJid, {
+        text: "I need to be an admin to change the group description.",
+      });
+    }
+    const newName = args.join(" ");
+    if (!newName) {
+      return sock.sendMessage(remoteJid, {
+        text: "Provide a new group description.",
+      });
+    }
+    try {
+      await sock.groupUpdateDescription(remoteJid, newName);
+      await sock.sendMessage(remoteJid, { text: "Group description updated." });
+    } catch (e) {
+      console.error("gcname error:", e);
+      await sock.sendMessage(remoteJid, {
+        text: "Failed to update group description.",
+      });
+    }
+  },
+};
