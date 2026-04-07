24d28d768022572b38859358b8d339a48b004cd3
Mellow <demmyjyd@gmail.com>
2026-04-07 20:27:06 +0000

    Add getsudo plugin

---
+++ plugins/getsudo.js
@@ -0,0 +1,13 @@
+import sudo from  "../data/sudo.json" with { type: "json" };
+
+export default {
+   name: "getsudo",
+   description: "Get sudo users",
+   isPublic: false,
+   category: "Owner",
+   execute: async (sock, msg, args) => {
+      const remoteJid = msg.key.remoteJid;
+      const sudoUsers = sudo.map((user) => `${user}`).join("\n");
+      await sock.sendMessage(remoteJid, { text: `Sudo users are: ${sudoUsers}` });
+   }
+}
