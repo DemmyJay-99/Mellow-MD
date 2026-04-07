7da3a458d1f0ce545b69e034168d7b16ebd84231
Mellow <demmyjyd@gmail.com>
2026-04-07 20:17:17 +0000

    Update
    Add restart plugin
    Add clearReact function

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,11 +1,16 @@
+import clearReact from "../lib/clearReact.js";
+
 export default {
-    name: "restart",
-    description: "Restart the bot",
-    isPublic: false,
-    category: "Bot",
-    execute: async (sock, msg, args) => {
-        const remoteJid = msg.key.remoteJid;
-        await sock.sendMessage(remoteJid, { text: "Restarting..." });
-        process.exit(0);
-    }
-}
+  name: "restart",
+  description: "Restart the bot",
+  isPublic: false,
+  category: "Bot",
+  execute: async (sock, msg, args) => {
+    const remoteJid = msg.key.remoteJid;
+    await sock.sendMessage(remoteJid, { text: "Restarting..." });
+    await clearReact(sock, msg);
+    setTimeout(() => {
+      process.exit(0);
+    }, 1500);
+  },
+};
