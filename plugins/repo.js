a1653d0270d25bef1a01efac97fd0ef38a32d508
Mellow <demmyjyd@gmail.com>
2026-04-09 21:44:14 +0000

    Add repo plugin

---
+++ plugins/repo.js
@@ -0,0 +1,22 @@
+export default {
+    name: "repo",
+    description: "Get the repository link of the bot",
+    isPublic: false,
+    category: "Bot",
+    execute: async (sock, msg, args) => {
+      const remoteJid = msg.key.remoteJid;
+      const link = "Repo link: https://github.com/DemmyJay-99/Mellow-MD.git\n\nDon't forget to give a star to the repo"
+      await sock.sendMessage(remoteJid, {
+          text: link,
+          contextInfo: {
+              externalAdReply: {
+                  title: "Mellow MD",
+                  thumbnailUrl: 'https://i.ibb.co/fVJQHczm/siGOdOA.jpg',
+                  sourceUrl: "https://github.com/DemmyJay-99/Mellow-MD.git",
+                  mediaType: 1,
+                  renderLargerThumbnail: true
+              }
+          }
+      })
+    }
+}
