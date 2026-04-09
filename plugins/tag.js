f9a60caedb4503e3727ba0668d1b5cd34dc503c4
Mellow <demmyjyd@gmail.com>
2026-04-09 11:20:01 +0000

    Update tag plugin

--- plugins/tag.js
+++ plugins/tag.js
@@ -3,7 +3,7 @@ export default {
    description: "Tag all members",
    isPublic: false,
    category: "Group",
-   execute: async (sock, msg, args) => {
+   execute: async (sock, msg, args, quotedMessage) => {
       const remoteJid = msg.key.remoteJid;
       if (!remoteJid.endsWith("@g.us")) {
          return sock.sendMessage(remoteJid, {
@@ -12,7 +12,57 @@ export default {
       }
       const metadata = await sock.groupMetadata(remoteJid);
       const members = metadata.participants.map((p) => p.id);
-      const mentionText = members.map((jid) => "@" + jid.split("@")[0]).join(" ");
-      await sock.sendMessage(remoteJid, { text: mentionText, mentions: members });
-   }
-}
+      const message = args.join(" ");
+      const repliedMessage =
+         quotedMessage?.conversation ||
+         quotedMessage?.extendedTextMessage?.text;
+      const options = ["all", "admins", "nonadmins"];
+      if (!message && !repliedMessage) {
+         return sock.sendMessage(remoteJid, {
+            text: "Provide a message to tag members with.",
+         });
+      } else if (message === options[0]) {
+         const mentions = members
+            .map((member) => `@${member.split("@")[0]}`)
+            .join("\n");
+         await sock.sendMessage(remoteJid, {
+            text: mentions,
+            mentions: members,
+         });
+      } else if (message === options[1]) {
+         const admins = metadata.participants
+            .filter((p) => p.admin)
+            .map((p) => p.id);
+         const mentions = admins
+            .map((admin) => `@${admin.split("@")[0]}`)
+            .join("\n");
+         await sock.sendMessage(remoteJid, {
+            text: mentions,
+            mentions: admins,
+         });
+      } else if (message === options[2]) {
+         const admins = metadata.participants
+            .filter((p) => p.admin)
+            .map((p) => p.id);
+         const nonAdmins = members.filter((member) => !admins.includes(member));
+         const mentions = nonAdmins
+            .map((nonAdmin) => `@${nonAdmin.split("@")[0]}`)
+            .join("\n");
+         await sock.sendMessage(remoteJid, {
+            text: mentions,
+            mentions: nonAdmins,
+         });
+      } else if (repliedMessage) {
+         console.log(repliedMessage);
+         await sock.sendMessage(remoteJid, {
+            text: `${repliedMessage}`,
+            mentions: members,
+         });
+      } else if (!options.includes(message)) {
+         await sock.sendMessage(remoteJid, {
+            text: `${message}`,
+            mentions: members,
+         });
+      }
+   },
+};
