a9bce860fa8fd9acf68b3e980bf56c1444e42aa2
Mellow <demmyjyd@gmail.com>
2026-04-10 12:10:23 +0000

    Cleanup antilink.js

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -1,46 +1,7 @@
-import fs from "fs";
 import normalizeJid from "../lib/normaliseLid.js";
+import { getGroupConfig, setGroupConfig, getWarns, setWarns } from "../lib/index.js";
 
-const ANTILINK_FILE = "./data/antilink.json";
-const WARN_FILE = "./data/warn.json";
 const WARN_LIMIT = Number(process.env.WARN_LIMIT) || 3;
-
-async function readData(FILE) {
-  if (!fs.existsSync("./data")) {
-    fs.mkdirSync("./data", { recursive: true });
-  }
-  if (!fs.existsSync(FILE)) {
-    fs.writeFileSync(FILE, JSON.stringify({}));
-  }
-  return JSON.parse(fs.readFileSync(FILE, "utf8"));
-}
-
-async function writeData(data, FILE) {
-  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
-}
-
-async function getGroupConfig(jid) {
-  const data = await readData(ANTILINK_FILE);
-  return data[jid] || { enabled: false, action: "delete" };
-}
-
-async function setGroupConfig(jid, config) {
-  const data = await readData(ANTILINK_FILE);
-  data[jid] = config;
-  await writeData(data, ANTILINK_FILE);
-}
-
-async function getWarns(jid, sender) {
-  const data = await readData(WARN_FILE);
-  return data[jid]?.[sender] || 0;
-}
-
-async function setWarns(jid, sender, count) {
-  const data = await readData(WARN_FILE);
-  if (!data[jid]) data[jid] = {};
-  data[jid][sender] = count;
-  await writeData(data, WARN_FILE);
-}
 
 export default {
   name: "antilink",
