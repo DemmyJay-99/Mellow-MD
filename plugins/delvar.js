4e21546ef77d1d745542e536d50a5f0f07f26336
Mellow <demmyjyd@gmail.com>
2026-04-12 23:33:42 +0000

    Add delvar plugin

---
+++ plugins/delvar.js
@@ -0,0 +1,18 @@
+import {delvar} from "../lib/index.js"
+
+export  default {
+   name: "delvar",
+   description: "Delete environment variables",
+   isPublic: false,
+   category: "Owner",
+   usage: "delvar <variable>",
+   execute: async (sock, msg, args) => {
+      const variable = args[0];
+      if (!variable) {
+         await sock.sendMessage(msg.key.remoteJid, {text: "Please provide a variable."});
+         return;
+      }
+      await delvar(variable);
+      await sock.sendMessage(msg.key.remoteJid, {text: `Deleted ${variable.toUpperCase()}`});
+   }
+}
