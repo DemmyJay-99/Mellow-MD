6028bfdbd3dbf296e1cdc3a4cdaeeebed6cfdccd
Mellow <demmyjyd@gmail.com>
2026-04-12 23:47:02 +0000

    Add allvars plugin

---
+++ plugins/allvars.js
@@ -0,0 +1,14 @@
+import { allvars } from "../lib/index.js";
+
+export default {
+  name: "allvars",
+  description: "Get all environment variables",
+  isPublic: false,
+  category: "Owner",
+  usage: "allvars",
+  execute: async (sock, msg, args) => {
+    const vars = await allvars();
+    const text = Object.entries(vars).map(([key, value]) => `${key} = ${value}`).join("\n");
+    await sock.sendMessage(msg.key.remoteJid, {text: text});
+  }
+};
