edae6a86aacb4dcccbc8a117820bb988d13e0bcd
DemmyJay <demmyjyd@gmail.com>
2026-04-16 12:32:20 +0100

    Fix mp3

--- plugins/mp3.js
+++ plugins/mp3.js
@@ -13,7 +13,7 @@ export default {
           text: "Reply to a video or document message.",
         });
       }
-      const {downloadContentFromMessage} = await import("baileys");
+      const {downloadContentFromMessage} = await import("@innovatorssoft/baileys");
       const stream = await downloadContentFromMessage(mediaMessage, "video");
       const chunks = [];
       for await (const chunk of stream) {
