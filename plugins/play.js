5f757b63360a8d083ae2f8cb481f2338310a94e4
DemmyJay <demmyjyd@gmail.com>
2026-04-24 13:04:00 +0100

    Update play

--- plugins/play.js
+++ plugins/play.js
@@ -1,5 +1,6 @@
 import yts from "yt-search";
 import axios from "axios";
+import {transform, getFonts} from "convert-unicode-fonts";
 export default {
   name: "play",
   description: "Play a song from YouTube",
@@ -18,10 +19,19 @@ export default {
     }
     const response = await axios.get(`https://api-rebix.zone.id/api/ytdl?format=mp3&url=${url}`);
     const {result} = response.data;
-    const songUrl = result?.download;
+    const {download, title, thumbnail, size} = result;
+
     if (!songUrl) {
       return sock.sendMessage(msg.key.remoteJid, {text: "Failed to retrieve the song."});
     }
-    await sock.sendMessage(msg.key.remoteJid, {audio: {url: songUrl}, mimetype: "audio/mpeg"});
+    const fonts = getFonts();
+    const transformedTitle = transform(title, fonts["script-bold"]);
+    const mellow = transform("Mellow-MD✨".toUpperCase(), fonts["script-bold"]);
+    const message = `${mellow}\nDownloading *${transformedTitle}*\nSize: ${size}`;
+    const m = await sock.sendMessage(msg.key.remoteJid, {
+      image: {url: thumbnail},
+      caption: message,
+    });
+    await sock.sendMessage(msg.key.remoteJid, {audio: {url: songUrl}, mimetype: "audio/mpeg"}, {quoted: m});
   },
 };
