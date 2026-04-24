0f1e74f5338ad3b96d55488d141454285c1bce29
DemmyJay <demmyjyd@gmail.com>
2026-04-24 13:27:56 +0100

    Update play

--- plugins/play.js
+++ plugins/play.js
@@ -13,25 +13,27 @@ export default {
       return sock.sendMessage(msg.key.remoteJid, {text: "Please provide a song name."});
     }
     const results = await yts(query);
-    const url = results.videos.length > 0 ? results.all[0]?.url : null;
+    const video = results.videos.length > 0 ? results.all[0] : null;
+    const url = video ? video.url : null;
     if (!url) {
       return sock.sendMessage(msg.key.remoteJid, {text: "No results found."});
     }
-    const response = await axios.get(`https://api-rebix.zone.id/api/ytdl?format=mp3&url=${url}`);
-    const {result} = response.data;
-    const {download, title, thumbnail, size} = result;
-
-    if (!songUrl) {
-      return sock.sendMessage(msg.key.remoteJid, {text: "Failed to retrieve the song."});
-    }
+    const {title, thumbnail, timestamp} = video;
     const fonts = getFonts();
     const transformedTitle = transform(title, fonts["script-bold"]);
     const mellow = transform("Mellow-MD✨".toUpperCase(), fonts["script-bold"]);
-    const message = `${mellow}\nDownloading *${transformedTitle}*\nSize: ${size}`;
+    const message = `${mellow}\nDownloading *${transformedTitle}*\nDuration: ${timestamp}`;
     const m = await sock.sendMessage(msg.key.remoteJid, {
       image: {url: thumbnail},
       caption: message,
     });
-    await sock.sendMessage(msg.key.remoteJid, {audio: {url: songUrl}, mimetype: "audio/mpeg"}, {quoted: m});
+    const response = await axios.get(`https://api-rebix.zone.id/api/ytdl?format=mp3&url=${url}`);
+    const {result} = response.data;
+    const {download} = result;
+
+    if (!download) {
+      return sock.sendMessage(msg.key.remoteJid, {text: "Failed to retrieve the song."});
+    }
+    await sock.sendMessage(msg.key.remoteJid, {audio: {url: download}, mimetype: "audio/mpeg"}, {quoted: m});
   },
 };
