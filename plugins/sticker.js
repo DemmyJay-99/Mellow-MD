b80eafcd0a1ef4ded0e9d7fb77b46d557a41bed0
DemmyJay <demmyjyd@gmail.com>
2026-04-26 14:31:05 +0100

    Update sticker plugin

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -17,11 +17,6 @@ export default {
     }
     let type = quotedMessage?.imageMessage ? "image" : quotedMessage?.videoMessage ? "video" : "document";
 
-    const isGif = quotedMessage?.videoMessage?.gifPlayback;
-    if(isGif) {
-      type = 'gif'
-    }
-
     const stream = await downloadContentFromMessage(mediaMessage, type);
     const chunks = [];
     for await (const chunk of stream) {
