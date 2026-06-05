d67526f196b2cc8221ed3bd54f197156075c1d51
Mellow <demmyjyd@gmail.com>
2026-06-05 03:03:51 +0000

    Fix

--- plugins/fb.js
+++ plugins/fb.js
@@ -30,7 +30,7 @@ export default {
       return;
     }
     const res = await fbdl(url);
-    const video = res.data[0]?.url;
+    const video = res[0]
     await sock.sendMessage(chatID, {video: {url: video}});
   },
 };
