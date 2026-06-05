360fd8368bf6523f092af5b786bed950e5aceb56
DemmyJay <demmyjyd@gmail.com>
2026-06-05 03:50:51 +0100

    Fix

--- plugins/disable.js
+++ plugins/disable.js
@@ -5,27 +5,27 @@ export default {
   description: "Disable a command for a specific group",
   category: "Group",
   usage: "disable <command>",
-  execute: async (sock, msg, args) => {
-    const remoteJid = msg.key.remoteJid;
-    if (!remoteJid.endsWith("@g.us")) {
-      return await sock.sendMessage(remoteJid, {
+  execute: async (sock, msg, args, mellow = {}) => {
+    const { chatID, chatIDisGroup } = mellow;
+    if (!chatIDisGroup) {
+      return await sock.sendMessage(chatID, {
         text: "This command can only be used in groups",
       });
     }
     if (!args[0]) {
-      return sock.sendMessage(remoteJid, {
+      return sock.sendMessage(chatID, {
         text: "Please provide a command name.\nExample: .disable ping",
       });
     }
     const command = args[0].toLowerCase();
-    const current = getEnabled(remoteJid);
+    const current = getEnabled(chatID);
     if (!current.includes(command)) {
-      return sock.sendMessage(remoteJid, {
+      return sock.sendMessage(chatID, {
         text: `*${command}* isn't enabled in this group.`,
       });
     }
-    disableCommand(remoteJid, command);
-    await sock.sendMessage(remoteJid, {
+    disableCommand(chatID, command);
+    await sock.sendMessage(chatID, {
       text: `*${command}* command has been disabled for this group`
     })
   },
--- plugins/gcdesc.js
+++ plugins/gcdesc.js
@@ -1,47 +1,40 @@
-import normaliseLid from "../lib/normaliseLid.js";
-
+import normaliseJidToPN from "../lib/normaliseJidToPN";
 export default {
   name: "gcdesc",
   description: "Change the group description",
   category: "Group",
   usage: "gcdesc <new description>",
-  execute: async (sock, msg, args) => {
-    const remoteJid = msg.key.remoteJid;
-    if (!remoteJid.endsWith("@g.us")) {
-      return sock.sendMessage(remoteJid, {
+  execute: async (sock, msg, args, mellow = {}) => {
+    const { chatID, senderID, botID } = mellow;
+    if (!chatID.endsWith("@g.us")) {
+      return sock.sendMessage(chatID, {
         text: "This command only works in groups.",
       });
     }
-    const metadata = await sock.groupMetadata(remoteJid);
-    let senderJid = msg.key.participant || msg.key.remoteJid;
-    if (senderJid.endsWith("@lid")) {
-      let pn = await normaliseLid(sock, senderJid);
-      senderJid = pn + "@s.whatsapp.net";
-    }
+    const metadata = await sock.groupMetadata(chatID);
+    const senderJid = await normaliseJidToPN(senderID);
     const admins = metadata.participants.filter((p) => p.admin).map((p) => p.id);
     if (!admins.includes(senderJid) && !msg.key.fromMe) {
-      return sock.sendMessage(remoteJid, {text: "Admin only."});
+      return sock.sendMessage(chatID, {text: "Admin only."});
     }
 
-    const botJid = sock.user.id.split(":")[0] + "@s.whatsapp.net";
-
-    if (!admins.includes(botJid)) {
-      return sock.sendMessage(remoteJid, {
+    if (!admins.includes(botID)) {
+      return sock.sendMessage(chatID, {
         text: "I need to be an admin to change the group description.",
       });
     }
     const newName = args.join(" ");
     if (!newName) {
-      return sock.sendMessage(remoteJid, {
+      return sock.sendMessage(chatID, {
         text: "Provide a new group description.",
       });
     }
     try {
-      await sock.groupUpdateDescription(remoteJid, newName);
-      await sock.sendMessage(remoteJid, {text: "Group description updated."});
+      await sock.groupUpdateDescription(chatID, newName);
+      await sock.sendMessage(chatID, {text: "Group description updated."});
     } catch (e) {
       console.error("gcname error:", e);
-      await sock.sendMessage(remoteJid, {
+      await sock.sendMessage(chatID, {
         text: "Failed to update group description.",
       });
     }
