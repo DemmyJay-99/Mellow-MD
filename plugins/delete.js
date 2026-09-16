671a0a8a9122993e97310dd9cb1cb9258ec1bb78
DemmyJay <demmyjyd@gmail.com>
2026-09-16 01:34:13 +0100

    feat: add delete plugin to remove quoted messages

---
+++ plugins/delete.js
@@ -0,0 +1,45 @@
+import { isSenderAdmin } from "../lib/index.js";
+
+export default {
+  name: "delete",
+  description: "Delete quoted message",
+  category: "Utility",
+  usage: "Reply to a message with delete",
+  execute: async (sock, msg, args, mellow = {}) => {
+    const { chatID, chatIDisGroup, senderID, botID, msgID, ctxInfo } = mellow;
+    try {
+      if (chatIDisGroup) {
+        const senderIsAdmin = await isSenderAdmin(sock, senderID, chatID);
+        if (!senderIsAdmin) {
+          return await sock.sendMessage(chatID, { text: "Admin Only" });
+        }
+        const botIsAdmin = await isSenderAdmin(sock, botID, chatID);
+        if (!botIsAdmin) {
+          return await sock.sendMessage(chatID, { text: "Admin Only" });
+        }
+        await sock.sendMessage(chatID, {
+          delete: {
+            remoteJid: chatID,
+            fromMe: false,
+            id: ctxInfo.stanzaId,
+            participant: ctxInfo.participant,
+          },
+        });
+        await sock.sendMessage(chatID, { text: "Deleted" });
+      } else {
+        const ddd = botID === ctxInfo.participant;
+        await sock.sendMessage(chatID, {
+          delete: {
+            remoteJid: chatID,
+            fromMe: ddd ? ddd : false,
+            id: ctxInfo.stanzaId,
+            participant: ctxInfo.participant,
+          },
+        });
+        await sock.sendMessage(chatID, { text: "Deleted" });
+      }
+    } catch (error) {
+      console.log("Failed to delete message");
+    }
+  },
+};
